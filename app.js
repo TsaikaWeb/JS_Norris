dbb2c9823453905de8450f5d2c310d8f299d2628
Opilane TTHK <opilane@tthk.ee>
2019-09-05 12:25:44 +0300

    commit

---
+++ app.js
@@ -0,0 +1,24 @@
+document.querySelector(".get-Jokes").addEventListener("click", getJokes);
+
+function getJokes(e) {
+    const number = document.querySelector('input[type=number]').value;
+    
+    fetch(`http://api.icndb.com/jokes/random/${number}`)
+    .then(function(response){
+        console.log(response);
+        return response.json();
+    })
+    .then(function(data){
+        let output = "";
+        
+        data.value.forEach(joke => {
+            output += `<li>${joke.joke}</li>`;
+            console.log(output);
+        });
+        document.querySelector(".jokes").innerHTML = output;
+
+    })
+
+    ;
+    e.preventDefault();
+}
